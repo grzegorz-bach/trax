f76c5d1497bab8a58f8b9e30c993dcf8cd2ab6f0
Grzegorz Bach <g.bach90@gmail.com>
2022-04-20 16:01:28 +0200

    display top playlists on the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,59 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+import { Artist } from "@prisma/client";
 import GradientLayout from "../components/GradientLayout";
+import prisma from "../lib/prisma";
 
-const Home = () => {
+interface Props {
+  artists: Artist[];
+}
+
+const Home = ({ artists }: Props) => {
   return (
     <GradientLayout
-      color="red"
+      color="purple"
       subtitle="profile"
       title="Scott Moss"
       description="15 public playlists"
       image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
       roundImage
     >
-      <div>Home</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artists this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex columnGap="10px" rowGap="10px" wrap="wrap">
+          {artists.map((artist) => (
+            <Box
+              bg="gray.900"
+              borderRadius="4px"
+              padding="15px"
+              flexBasis="15%"
+            >
+              <Image src="https://placekitten.com/300/300" borderRadius="50%" />
+              <Box marginTop="20PX">
+                <Text fontSize="large">{artist.name}</Text>
+                <Text fontSize="x-small">Artist</Text>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: {
+      artists,
+    },
+  };
 };
 
 export default Home;
