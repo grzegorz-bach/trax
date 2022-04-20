77050306e57ef6a316c38a2d5b792602265a9c7c
Grzegorz Bach <g.bach90@gmail.com>
2022-04-20 15:28:42 +0200

    add gardient layout header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import GradientLayout from "../components/GradientLayout";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <GradientLayout
+      color="red"
+      subtitle="profile"
+      title="Scott Moss"
+      description="15 public playlists"
+      image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
+      roundImage
+    >
+      <div>Home</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
