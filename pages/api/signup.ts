1375ba6b7e72c039bd5eb7fa147d0613b5faf9bd
Grzegorz Bach <g.bach90@gmail.com>
2022-04-24 11:05:12 +0200

    fix typescript error

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -11,7 +11,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   const salt = bcrypt.genSaltSync();
-  const { email, password } = req.body;
+  const { email, password, firstName, lastName } = req.body;
 
   let user;
 
@@ -20,6 +20,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       data: {
         email,
         password: bcrypt.hashSync(password, salt),
+        firstName: firstName || "",
+        lastName: lastName || "",
       },
     });
   } catch (e) {
